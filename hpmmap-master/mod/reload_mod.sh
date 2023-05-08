55b73b47cbb85d5768b57d1333efc779e7bfc7c4
ikn1062 <ishaannarain2022@u.northwestern.edu>
2023-05-08 17:16:39 -0500

    Making changes to run on Linux 4.x with debug

--- hpmmap-master/mod/reload_mod.sh
+++ hpmmap-master/mod/reload_mod.sh
@@ -10,8 +10,9 @@ fi
 
 # Load module - lookup addr of syscall table
 insmod hpmmap.ko syscall_table_addr=0x$(grep -i "r sys_call_table" /proc/kallsyms | sed 's/\([0-9a-f]*\) [rR] sys_call_table/\1/') cur_kprobe=0x$(grep current_kprobe /proc/kallsyms | cut -d " " -f 1 -z)
+echo "insmod successful"
 
 # Optionally, set hpmmap permissions to avoid running applications as root
 sleep 2
-chgrp ishaann /dev/hpmmap 
+chgrp ishaan1604 /dev/hpmmap 
 chmod 666 /dev/hpmmap
