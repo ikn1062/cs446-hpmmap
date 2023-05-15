0e47ded8589df7210add742216199f42cb4d09ca
ikn1062 <ishaannarain2022@u.northwestern.edu>
2023-05-15 11:55:10 -0500

    Fixing test files

--- hpmmap-master/test/test.c
+++ hpmmap-master/test/test.c
@@ -1,3 +1,4 @@
+#include <linux/module.h>
 #include <asm/unistd.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
@@ -5,6 +6,10 @@
 #include <linux/kprobes.h>
 #include <linux/mm.h>
 #include <linux/preempt.h>
+#include <linux/init.h>
+#include <linux/syscalls.h>
+
+MODULE_LICENSE("GPL");
 
 
 int kallsyms_lookup_name_fn(const char *lookup_name, unsigned long *ret_address)
@@ -15,7 +20,7 @@ int kallsyms_lookup_name_fn(const char *lookup_name, unsigned long *ret_address)
 
     int check_reg = register_kprobe(&kp);
     if (check_reg < 0) {
-        PrintError("failed to register kprobe for %s, returned %d\n", lookup_name, check_reg);
+        //PrintError("failed to register kprobe for %s, returned %d\n", lookup_name, check_reg);
         return check_reg;
     }
     *ret_address = (unsigned long) kp.addr;
@@ -24,7 +29,7 @@ int kallsyms_lookup_name_fn(const char *lookup_name, unsigned long *ret_address)
     return 0;
 }
 
-int main()
+int __init test_init(void) 
 {
     int err = 0;
     unsigned long add;
@@ -33,3 +38,12 @@ int main()
     
     return 0;
 }
+
+static void __exit
+test_exit(void ) 
+{
+
+}
+
+module_init(test_init);
+module_exit(test_exit);
