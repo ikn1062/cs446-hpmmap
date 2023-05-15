7dc46ac43e1b89012bc181a3c644b5d155d162ca
ikn1062 <ishaannarain2022@u.northwestern.edu>
2023-05-15 12:47:38 -0500

    Got test working

--- hpmmap-master/test/test.c
+++ hpmmap-master/test/test.c
@@ -11,19 +11,32 @@
 
 MODULE_LICENSE("GPL");
 
+# define PrintDebug(fmt, args...) printk("HPMMAP (debug): " fmt, ## args)
+
+#define PrintError(fmt, args...) do {\
+    printk("HPMMAP (error at %s:%d): %s: ", __FILE__, __LINE__, __func__);\
+    printk(fmt, ## args);\
+} while(0)
+
+typedef unsigned long (*kallsyms_lookup_name_t)(const char *name);
+kallsyms_lookup_name_t kallsyms_lookup_name_test;
 
 int kallsyms_lookup_name_fn(const char *lookup_name, unsigned long *ret_address)
 {
     struct kprobe kp = {
         .symbol_name = lookup_name
     };
-
+    PrintDebug("Registering Kprobe");
     int check_reg = register_kprobe(&kp);
     if (check_reg < 0) {
-        //PrintError("failed to register kprobe for %s, returned %d\n", lookup_name, check_reg);
+        PrintError("failed to register kprobe for %s, returned %d\n", lookup_name, check_reg);
         return check_reg;
     }
     *ret_address = (unsigned long) kp.addr;
+
+    kallsyms_lookup_name_test = (kallsyms_lookup_name_t) kp.addr;
+
+    PrintDebug("Unregistering Kprobe");
     unregister_kprobe(&kp);
 
     return 0;
@@ -33,8 +46,14 @@ int __init test_init(void)
 {
     int err = 0;
     unsigned long add;
+    PrintDebug("Registering Krpobe for %s", "kallsyms_lookup_name");
+    err = kallsyms_lookup_name_fn("kallsyms_lookup_name", &add);
 
-    err = kallsyms_lookup_name_fn("current_kprobe", &add);
+    PrintDebug("Testing Kallsyms lookup name func");
+    unsigned long current_kprobe = kallsyms_lookup_name_test("current_kprobe");
+    PrintDebug("Got Kprobe address: 0x%p", current_kprobe);
+    PrintDebug("Got Kprobe address: 0x%lx", current_kprobe);
+    PrintDebug("Complete");
     
     return 0;
 }
