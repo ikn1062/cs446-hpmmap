51be3b5c9140dfc610545b35c508e95a48916aec
Ishaan Narain <ishaannarain2022@u.northwestern.edu>
2023-05-30 10:23:36 -0500

    Adding debug before memset

--- hpmmap-master/mod/mmap.c
+++ hpmmap-master/mod/mmap.c
@@ -779,6 +779,7 @@ do_hpmmap_mmap_anon(struct memory_state * state,
             break;
     }
 
+    PrintDebug("do-hpmmap-mmap-anon memset ret: %lu, len: %d", read, len);
     memset((void *)ret, 0, len);
     PrintDebug("do-hpmmap-mmap-anon return: %lu", addr);
     return ret;
